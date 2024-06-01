3868a56b6f5225dc1d07b2c14104d49d7e544404
ebowwa <81942069+ebowwa@users.noreply.github.com>
2024-06-01 21:40:42 +0000

    added clarity notes

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,3 @@
-from .resume_generator import ResumePDF, load_data
-from .json_generator import load_data 
+from .resume_generator import ResumePDF, load_data # backend to generate the PDF resume
+from .json_generator import load_data # GUI to edit JSON data
 # from .json_api import app, save_json  # install Flask and add this line
